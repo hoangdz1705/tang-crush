678163c77aff5dd0aba78e7bda3a33f58f1e7436
hoangdz1705 <86798335+hoangdz1705@users.noreply.github.com>
2021-07-02 01:15:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'chị iu à!',
+    introTitle: 'em à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao chị cứ đi nhầm đường
-    Lạc vào tim em lẻ loi
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phố đã lên đèn, con đường có môi kề môi, phố đã lên đèn, bên người em thương em nhớ e mong '
-    desc: 'có nhạc bên cạnh ai không feel, có chị bên cạnh yêu không phai ',
-    btnYes: 'chị yêu em <33',
-    btnNo: 'Không, em trai đáng iu à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao chị lại yêu em <3',
-    btnReply: 'Gửi cho em <3',
+    title: 'Phố đã lên đèn, con đường có môi kề môi, phố đã lên đèn, bên người anh thương anh nhớ anh mong '
+    desc: 'có nhạc bên cạnh ai không feel, có em bên cạnh yêu không phai ',
+    btnYes: 'em yêu anh <33',
+    btnNo: 'Không, anh trai đáng iu à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Em biết mà 🥰. Yêu chị Ngọc nhiều nhiều 😘😘',
-    messDesc: 'vậy chị trở thành nàng thưo của e .',
+    mess: 'anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'vậy em trở thành nàng thơ của anh nha .',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
