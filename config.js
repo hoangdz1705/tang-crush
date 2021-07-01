7ef42aff5962d602a17232d51dc6c12658bca7ea
hoangdz1705 <86798335+hoangdz1705@users.noreply.github.com>
2021-07-02 01:05:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,15 +5,15 @@ const CONFIG = {
     Lạc vào tim em lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích chị <33',
-    btnNo: 'Không, Chị Ngọc đáng iu à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu chị <3',
-    btnReply: 'Gửi cho chị <3',
+    title: 'Phố đã lên đèn, con đường có môi kề môi, phố đã lên đèn, bên người em thương em nhớ e mong '
+    desc: 'có nhạc bên cạnh ai không feel, có chị bên cạnh yêu không phai ',
+    btnYes: 'chị yêu em <33',
+    btnNo: 'Không, em trai đáng iu à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao chị lại yêu em <3',
+    btnReply: 'Gửi cho em <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Em biết mà 🥰. Yêu chị Ngọc nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'vậy chị trở thành nàng thưo của e .',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
